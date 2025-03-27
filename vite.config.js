9b77894850527878b23ee0452ea69b676824c9b0
teyms <teymingsheng@gmail.com>
2025-03-27 19:52:10 +0800

    apply cache busting to prevent browser caching the old UI

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: `[name].[hash].js`,  // This will add a hash to the chunk filenames
+        entryFileNames: `[name].[hash].js`,   // This will add a hash to the entry filenames
+        assetFileNames: `[name].[hash].[ext]`, // This will add a hash to the asset filenames
+      },
+    },
+  },
   // assetsInclude: ['**/*.html'],  // Add this line to include HTML files as assets
   // define:{
   //   'process.env.VITE_SELF_URL': JSON.stringify(process.env.VITE_SELF_URL),
